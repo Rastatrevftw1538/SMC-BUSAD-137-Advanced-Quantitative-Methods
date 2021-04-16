1cbfd0ca7f3978c739b7afb5636cd05978050a7c
Trevor Cardoza <trevor.cardoza1538@gmail.com>
2021-04-15 17:11:14 -0700

    finished hw

--- Homework #3/R Script HW3.R
+++ Homework #3/R Script HW3.R
@@ -10,4 +10,22 @@ Quantity
 
 InvoiceTimeStamp <- c("12/01/2010  08:26:00","05/08/2011  12:42:00","03/27/2011  04:08:00","03/28/2011  07:53:00","08/10/2011  05:11:00")
 InvoiceTimeStamp
- 
+
+
+Country <- c("US","Canada","France","US","France")
+Country
+
+CustomerID[1:4]
+
+UnitPrice[2:5]
+
+online_retail <- data.frame(CustomerID,UnitPrice,Quantity,InvoiceTimeStamp,Country)
+online_retail 
+
+dim(online_retail)
+
+online_retail[c(1,3,4,5),c(1,3,5)]
+
+data.frame(online_retail[2])
+
+c(online_retail[2])
